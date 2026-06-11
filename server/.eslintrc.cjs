2c9c1f9e50341370af7920db997bf1d0ac4747f7
Emape-g <emape.gianolini@gmail.com>
2026-06-11 00:04:53 -0300

    fix: config eslint para CI verde + quitar cd redundante

--- server/.eslintrc.cjs
+++ server/.eslintrc.cjs
@@ -9,4 +9,19 @@ module.exports = {
   ignorePatterns: ['dist'],
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
+  rules: {
+    // Permitir variables/argumentos/errores intencionalmente sin usar con prefijo "_"
+    // y descartes por rest-siblings (ej: const { password: _pw, ...rest } = user)
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      {
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
+        caughtErrorsIgnorePattern: '^_',
+        ignoreRestSiblings: true,
+      },
+    ],
+    // Permitir `declare global { namespace Express { ... } }` para augmentar tipos
+    '@typescript-eslint/no-namespace': ['error', { allowDeclarations: true }],
+  },
 }
